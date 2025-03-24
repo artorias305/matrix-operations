b2f815b7838b99139c1b564eaf1c038b0297d666
artorias305 <caique.andraus@gmail.com>
2025-03-24 11:45:07 -0300

    Implement matrix addition

--- main.c
+++ main.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 void getMatrix(int rows, int cols, int matrix[rows][cols]) {
-  printf("Enter the elements of the %dx%d matrix:\n", rows, cols);
+  printf("Enter the elements for a %dx%d matrix:\n", rows, cols);
   for (int i = 0; i < rows; i++) {
     for (int j = 0; j < cols; j++) {
       printf("Element [%d][%d]: ", i + 1, j + 1);
@@ -11,12 +11,20 @@ void getMatrix(int rows, int cols, int matrix[rows][cols]) {
 }
 
 void printMatrix(int rows, int cols, int matrix[rows][cols]) {
-  printf("The entered matrix is:\n");
+  printf("Matrix:\n");
   for (int i = 0; i < rows; i++) {
     for (int j = 0; j < cols; j++) {
       printf("%d\t", matrix[i][j]);
     }
     printf("\n");
+  }
+}
+
+void addMatrices(int rows, int cols, int mat1[rows][cols], int mat2[rows][cols], int result[rows][cols]) {
+  for (int i = 0; i < rows; i++) {
+    for (int j = 0; j < cols; j++) {
+      result[i][j] = mat1[i][j] + mat2[i][j];
+    }
   }
 }
 
@@ -28,11 +36,18 @@ int main() {
   printf("Enter the number of columns: ");
   scanf("%d", &cols);
 
-  int matrix[rows][cols];
+  int matrix1[rows][cols], matrix2[rows][cols], sum[rows][cols];
 
-  getMatrix(rows, cols, matrix);
+  printf("Enter the values for the first matrix:\n");
+  getMatrix(rows, cols, matrix1);
 
-  printMatrix(rows, cols, matrix);
+  printf("Enter the values for the second matrix:\n");
+  getMatrix(rows, cols, matrix2);
+
+  addMatrices(rows, cols, matrix1, matrix2, sum);
+
+  printf("Resultant matrix after addition:\n");
+  printMatrix(rows, cols, sum);
 
   return 0;
 }
